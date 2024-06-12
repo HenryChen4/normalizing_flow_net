d6fb71217837470110387589b2e4750ba169e198
Henry Chen <hchen365@usc.edu>
2024-06-12 02:11:23 -0700

    annotated and refactored

--- src/main.py
+++ src/main.py
@@ -8,12 +8,24 @@ from model import Normalizing_Flow_Net
 from visualize import visualize
 
 def sample_arm_input(arm_dim, seed):
-    """Sample initial arm pose from normal distribution"""
+    """Sample initial arm pose from normal distribution
+    
+    Args:
+        arm_dim (int): Number of arm joints.
+        seed (int): Rng seed.
+    Returns:
+        arm_solution (np.ndarray): Single sampled arm solution
+    """
     rng = np.random.default_rng(seed=seed)
     return rng.normal(loc=0.0, scale=np.pi/3, size=(arm_dim, ))
 
 def get_cartesian(arm_pos):
-    """Get cartesian coords from arm pos. Assumes link length = 1"""
+    """Get cartesian coords from arm pos. Link lengths are
+    assumed to be 1.
+    
+    Args:
+        arm_pos (np.ndarray): Arm joint position.
+    """
     theta_sum = 0
     x_final = 0
     y_final = 0
@@ -24,6 +36,16 @@ def get_cartesian(arm_pos):
     return (x_final, y_final)
 
 def generate_data(arm_dim, num_rows, random_sample_seed):
+    """Generates training data for neural net.
+
+    Args:
+        arm_dim (int): Number of arm joints.
+        num_rows (int): Size of dataset.
+        random_sample_seed (int): Random sample seed.
+    Returns:
+        data (list): Array containing features, cartesian x and y,
+        and targets, joint positions.
+    """
     data = []
     for i in range(num_rows):
         arm_solution = sample_arm_input(arm_dim=arm_dim, 
@@ -32,12 +54,13 @@ def generate_data(arm_dim, num_rows, random_sample_seed):
         data.append((arm_solution, car_x, car_y))
     return data
 
-data = generate_data(10, 10000, 1525)
+"""START of testing"""
+rng = np.random.default_rng(seed=315)
 
 # (hyper) parameters ??
 arm_dim = 10
 target_cartesian = np.array((5, 7))
-num_coupling_layers = 5
+num_coupling_layers = 1
 
 # seeds
 arm_gen_seed = 124
@@ -48,9 +71,12 @@ arm_link_lens = np.repeat(1, arm_dim)
 initial_arm_sample = sample_arm_input(arm_dim=arm_dim,
                                       seed=arm_gen_seed)
 
+# sample c, supposed to be sampled every epoch
+c = rng.uniform(0, 1)
+
 # initialize the flow net
 conditional_net_config = {
-    "layer_specs": [(arm_dim//2 + 2, 512),
+    "layer_specs": [(arm_dim//2 + 3, 512),
                     (512, 512),
                     (512, 512),
                     (512, 2)],
@@ -63,14 +89,19 @@ normalizing_flow_net = Normalizing_Flow_Net(conditional_net_config=conditional_n
 arm_solutions = normalizing_flow_net.forward(initial_arm_solutions=initial_arm_sample,
                              car_x=target_cartesian[0],
                              car_y=target_cartesian[1],
+                             c=c,
                              permute_seed=permute_seed)
+
+print(arm_solutions)
 
 # visualize final arm output
 fig, ax = plt.subplots(figsize=(8, 8))
 
-visualize(solution=arm_solutions[0].detach().numpy(),
+visualize(solution=arm_solutions.detach().numpy(),
           link_lengths=arm_link_lens,
           objective=10,
           ax=ax)
 
 plt.show()
+
+"""END of testing"""
--- src/model.py
+++ src/model.py
@@ -21,41 +21,77 @@ class Conditional_Net(nn.Module):
         return self.model(input)
     
 class Coupling_Layer:
+    """Transforms sampled arm solution through an affine
+    normalizing flow into a new arm solution.
+
+    Implemented as described in: "https://arxiv.org/pdf/2111.08933"
+
+    Args:
+        conditional_net (Conditional_Net): Neural network
+        for retrieving scaling (s) and transformation (t) 
+        values.
+    """
     def __init__(self, conditional_net):
         self.conditional_net = conditional_net
 
-    def get_s_t(self, in_arm_poses, in_cartesian_pose):
-        """Feed inputs into the conditional_net to get s and t.
+    def get_s_t(self, in_arm_poses, in_cartesian_pose, c):
+        """Performs affine flow calculations.
+
         Args:
-            cartesian_pose (np.ndarray): (x, y) coordinates for arms
-            in_arm_poses (np.ndarray): D/2 arms poses
+            in_arm_poses (torch.tensor): Input arm solutions.
+            in_cartesian_pose (torch.tensor): Input cartesian poses.
+            c (torch.tensor): Input c.
         Returns:
-            s, t gaussian transformation
+            s (torch.float32): Scaling factor.
+            t (torch.float32): Translation factor.
         """
         in_cartesian_pose = torch.squeeze(in_cartesian_pose)
-        conditional_input = torch.cat((in_arm_poses, in_cartesian_pose), dim=-1)
+        c = c.unsqueeze(dim=0)
+        conditional_input = torch.cat((in_arm_poses, in_cartesian_pose, c), dim=-1)
         layer_out = self.conditional_net(conditional_input)
         s, t = layer_out.chunk(2, dim=-1)
         return s, t
 
-    def forward(self, arm_solutions, car_x, car_y):
-        d = arm_solutions.shape[0]
-        const_arm_soln = arm_solutions[0:d//2]
-        altered_arm_soln = arm_solutions[d//2:d]
-
-        car_pose = torch.tensor([car_x, car_y], dtype=torch.float32).unsqueeze(0)
+    def forward(self, arm_solution, car_x, car_y, c):
+        """Forward propagate single input.
         
-        s, t = self.get_s_t(const_arm_soln, car_pose)
+        Args:
+            arm_solution (torch.tensor): Single sampled arm solution.
+            car_x (float): Target cartesian x.
+            car_y (float): Target cartesian y.
+        Returns:
+            new_arm_solution (torch.tensor): Transformed arm solution.
+            s (torch.float32): Scaling factor.
+        """
+        d = arm_solution.shape[0]
+        const_arm_soln = arm_solution[0:d//2]
+        altered_arm_soln = arm_solution[d//2:d]
+
+        car_pose = torch.tensor([car_x, car_y], dtype=torch.float32)
+        c = torch.tensor(c, dtype=torch.float32)
+        
+        s, t = self.get_s_t(const_arm_soln, car_pose, c)
         altered_arm_soln = altered_arm_soln * torch.exp(s) + t
 
         return torch.cat((const_arm_soln, altered_arm_soln), dim=0), s
 
 class Permute_Layer:
+    """Permutes arm solutions for input into next layer/unit.
+    
+    Args:
+        arm_soln (torch.tensor): Arm solution to be permuted.
+        seed (int): Permute seed
+    """
     def __init__(self, arm_soln, seed):
         self.arm_soln = arm_soln
         self.seed = seed
 
     def forward(self):
+        """Feed arm solution forward.
+
+        Returns:
+            permuted_arm_solution (torch.tensor): Permuted arm solution.
+        """
         rng = np.random.default_rng(seed=self.seed)
         permuted = rng.permutation(self.arm_soln.detach().numpy())
         return torch.tensor(permuted, dtype=torch.float32)
@@ -66,13 +102,13 @@ class Normalizing_Flow_Net(nn.Module):
         self.conditional_net = Conditional_Net(**conditional_net_config)
         self.num_layers = num_layers
     
-    def forward(self, initial_arm_solutions, car_x, car_y, permute_seed):
+    def forward(self, initial_arm_solutions, car_x, car_y, c, permute_seed):
         arm_solutions = torch.tensor(initial_arm_solutions, dtype=torch.float32)
 
         for i in range(self.num_layers):
             # first feed through coupling
             c_layer = Coupling_Layer(self.conditional_net)
-            arm_solutions, s = c_layer.forward(arm_solutions, car_x, car_y)
+            arm_solutions, s = c_layer.forward(arm_solutions, car_x, car_y, c)
 
             # permute the solutions
             p_layer = Permute_Layer(arm_solutions, permute_seed)
