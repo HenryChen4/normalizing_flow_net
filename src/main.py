4b14ae72f8ba26fc76ef88a8a725e7f708cbe17b
Henry Chen <hchen365@usc.edu>
2024-06-18 23:36:55 -0700

    higher lr

--- src/main.py
+++ src/main.py
@@ -34,7 +34,7 @@ data_loader = load_data(data=training_data,
 
 # different hyperparameters
 num_iters = 1000
-learning_rate = 1e-7
+learning_rate = 3e-7
 num_coupling_layers = 4
 noise_scale = 1e-3
 
