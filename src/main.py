b4876940d8e58002dfc9f3841f40e925e1080ad6
Henry Chen <hchen365@usc.edu>
2024-06-19 00:55:25 -0700

    experiment for night

--- src/main.py
+++ src/main.py
@@ -15,8 +15,8 @@ from model_eval import evaluate, evaluate_seeds
 from tqdm import tqdm, trange
 
 # data generation vars
-arm_dim = 2
-num_train_samples = 32000
+arm_dim = 10
+num_train_samples = 64000
 batch_size = 16
 
 # seeds
@@ -34,50 +34,51 @@ data_loader = load_data(data=training_data,
 
 # different hyperparameters
 num_iters = 10
-learning_rate = 5e-4
-num_coupling_layers = 4
+learning_rate = 1e-4
+num_coupling_layers = [6, 10, 15]
 noise_scale = 1e-3
 
 # model creation
 conditional_net_config = {
-    "layer_specs": [(arm_dim//2 + 3, 64),
-                    (64, 64),
-                    (64, arm_dim)],
+    "layer_specs": [(arm_dim//2 + 3, 256),
+                    (256, 256),
+                    (256, arm_dim)],
     "activation": nn.LeakyReLU,
 }
 
-# main experiment loop
-normalizing_flow_net = Normalizing_Flow_Net(conditional_net_config=conditional_net_config,
-                                            noise_scale=noise_scale,
-                                            num_layers=num_coupling_layers)
+for i in num_coupling_layers:
+    # main experiment loop
+    normalizing_flow_net = Normalizing_Flow_Net(conditional_net_config=conditional_net_config,
+                                                noise_scale=noise_scale,
+                                                num_layers=i)
 
-# model training
-all_epoch_loss, all_mean_dist = normalizing_flow_net.train(arm_dim=arm_dim,
-                                                           data_loader=data_loader,
-                                                           num_iters=num_iters,
-                                                           optimizer=Ranger,
-                                                           learning_rate=learning_rate,
-                                                           batch_size=batch_size,
-                                                           c_seed=c_seed,
-                                                           permute_seed=PERMUTE_SEED)
+    # model training
+    all_epoch_loss, all_mean_dist = normalizing_flow_net.train(arm_dim=arm_dim,
+                                                            data_loader=data_loader,
+                                                            num_iters=num_iters,
+                                                            optimizer=Ranger,
+                                                            learning_rate=learning_rate,
+                                                            batch_size=batch_size,
+                                                            c_seed=c_seed,
+                                                            permute_seed=PERMUTE_SEED)
 
-all_mean_dist = [dist.cpu().numpy() for dist in all_mean_dist]
+    all_mean_dist = [dist.cpu().numpy() for dist in all_mean_dist]
 
-# save results
-save_dir = f"results/2d_arm/"
-os.makedirs(save_dir, exist_ok=True)
-epoch_loss_save_path = os.path.join(save_dir, 'epoch_loss7e-7.png')
-dist_save_path = os.path.join(save_dir, 'mean_dist7e-7.png')
-model_save_path = os.path.join(save_dir, 'model7e-7.pth')
+    # save results
+    save_dir = f"results/2d_arm/"
+    os.makedirs(save_dir, exist_ok=True)
+    epoch_loss_save_path = os.path.join(save_dir, f'epoch_loss_{i}_cl.png')
+    dist_save_path = os.path.join(save_dir, f'mean_dist_{i}_cl.png')
+    model_save_path = os.path.join(save_dir, f'model_{i}_cl.pth')
 
-plt.plot(np.arange(num_iters), all_epoch_loss)
-plt.savefig(epoch_loss_save_path)
-plt.show()
-plt.clf()
+    plt.plot(np.arange(num_iters), all_epoch_loss)
+    plt.savefig(epoch_loss_save_path)
+    plt.show()
+    plt.clf()
 
-plt.plot(np.arange(num_iters), all_mean_dist)
-plt.savefig(dist_save_path)
-plt.show()
-plt.clf()
+    plt.plot(np.arange(num_iters), all_mean_dist)
+    plt.savefig(dist_save_path)
+    plt.show()
+    plt.clf()
 
-torch.save(normalizing_flow_net, model_save_path)
+    torch.save(normalizing_flow_net, model_save_path)
