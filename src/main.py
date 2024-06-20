dc3479774a7e9e32068b4762a1fc35f3967f3ea9
Henry Chen <hchen365@usc.edu>
2024-06-19 22:22:46 -0700

    ludricous numbers

--- src/main.py
+++ src/main.py
@@ -35,51 +35,49 @@ data_loader = load_data(data=training_data,
 # different hyperparameters
 num_iters = 200
 learning_rate = 1e-2
-num_coupling_layers = [3, 1, 6]
-noise_scale = [10, 2, 5]
+num_coupling_layers = 50
+noise_scale = 100
 
 # model creation
 conditional_net_config = {
     "layer_specs": [(arm_dim//2 + 3, 16),
                     (16, 16),
                     (16, arm_dim)],
-    "activation": nn.SELU,
+    "activation": nn.LeakyReLU,
 }
 
-for j in noise_scale:
-    for i in num_coupling_layers:
-        # main experiment loop
-        normalizing_flow_net = Normalizing_Flow_Net(conditional_net_config=conditional_net_config,
-                                                    noise_scale=j,
-                                                    num_layers=i)
+# main experiment loop
+normalizing_flow_net = Normalizing_Flow_Net(conditional_net_config=conditional_net_config,
+                                            noise_scale=noise_scale,
+                                            num_layers=num_coupling_layers)
 
-        # model training
-        all_epoch_loss, all_mean_dist = normalizing_flow_net.train(arm_dim=arm_dim,
-                                                                data_loader=data_loader,
-                                                                num_iters=num_iters,
-                                                                optimizer=Ranger,
-                                                                learning_rate=learning_rate,
-                                                                batch_size=batch_size,
-                                                                c_seed=c_seed,
-                                                                permute_seed=PERMUTE_SEED)
+# model training
+all_epoch_loss, all_mean_dist = normalizing_flow_net.train(arm_dim=arm_dim,
+                                                        data_loader=data_loader,
+                                                        num_iters=num_iters,
+                                                        optimizer=Ranger,
+                                                        learning_rate=learning_rate,
+                                                        batch_size=batch_size,
+                                                        c_seed=c_seed,
+                                                        permute_seed=PERMUTE_SEED)
 
-        all_mean_dist = [dist.cpu().numpy() for dist in all_mean_dist]
+all_mean_dist = [dist.cpu().numpy() for dist in all_mean_dist]
 
-        # save results
-        save_dir = f"results/2d_arm/"
-        os.makedirs(save_dir, exist_ok=True)
-        epoch_loss_save_path = os.path.join(save_dir, f'epoch_loss_{i}cl_{j}ns.png')
-        dist_save_path = os.path.join(save_dir, f'mean_dist_{i}cl_{j}ns.png')
-        model_save_path = os.path.join(save_dir, f'model_{i}cl_{j}ns.pth')
+# save results
+save_dir = f"results/2d_arm/"
+os.makedirs(save_dir, exist_ok=True)
+epoch_loss_save_path = os.path.join(save_dir, f'epoch_loss_{num_coupling_layers}cl_{noise_scale}ns.png')
+dist_save_path = os.path.join(save_dir, f'mean_dist_{num_coupling_layers}cl_{noise_scale}ns.png')
+model_save_path = os.path.join(save_dir, f'model_{num_coupling_layers}cl_{noise_scale}ns.pth')
 
-        plt.plot(np.arange(num_iters), all_epoch_loss)
-        plt.savefig(epoch_loss_save_path)
-        plt.show()
-        plt.clf()
+plt.plot(np.arange(num_iters), all_epoch_loss)
+plt.savefig(epoch_loss_save_path)
+plt.show()
+plt.clf()
 
-        plt.plot(np.arange(num_iters), all_mean_dist)
-        plt.savefig(dist_save_path)
-        plt.show()
-        plt.clf()
+plt.plot(np.arange(num_iters), all_mean_dist)
+plt.savefig(dist_save_path)
+plt.show()
+plt.clf()
 
-        torch.save(normalizing_flow_net, model_save_path)
+torch.save(normalizing_flow_net, model_save_path)
