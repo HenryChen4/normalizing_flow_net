839658d8ebc5166d5d2184c6a89dd84828390c30
Henry Chen <hchen365@usc.edu>
2024-06-20 23:58:20 -0700

    trying diff seed

--- src/main.py
+++ src/main.py
@@ -20,9 +20,9 @@ num_train_samples = 3200
 batch_size = 16
 
 # seeds
-train_sample_gen_seed = 907809284093457
-c_seed = 8923486023452
-PERMUTE_SEED = 28345023406623450 # constant for both training and testing
+train_sample_gen_seed = 72348967892345
+c_seed = 23495798023458
+PERMUTE_SEED = 283623450 # constant for both training and testing
 
 # sampling and loading dataset
 training_data = generate_data(arm_dim=arm_dim,
